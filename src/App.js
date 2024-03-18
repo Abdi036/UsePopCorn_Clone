363b711812c8a599bba3ab1622ea37c6a1ac5aa0
Abdi kumela <abdikumela@036gmail.com>
2024-03-18 14:10:03 +0300

    navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,89 @@
+/*const tempMovieData = [
+  {
+    imdbID: "tt1375666",
+    Title: "Inception",
+    Year: "2010",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+  },
+  {
+    imdbID: "tt0133093",
+    Title: "The Matrix",
+    Year: "1999",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
+  },
+  {
+    imdbID: "tt6751668",
+    Title: "Parasite",
+    Year: "2019",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg",
+  },
+];*/
+
+/*const tempWatchedData = [
+  {
+    imdbID: "tt1375666",
+    Title: "Inception",
+    Year: "2010",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+    runtime: 148,
+    imdbRating: 8.8,
+    userRating: 10,
+  },
+  {
+    imdbID: "tt0088763",
+    Title: "Back to the Future",
+    Year: "1985",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
+    runtime: 116,
+    imdbRating: 8.5,
+    userRating: 9,
+  },
+];*/
+
 export default function App() {
   return (
+    <>
+      <Navbar />
+    </>
+  );
+}
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <Logo />
+      <Search />
+      <MovieQuantity />
+    </nav>
+  );
+}
+
+function Logo() {
+  return (
+    <div className="logo">
+      <span>🎬</span>
+      <h1>UsePopCorn</h1>
+    </div>
+  );
+}
+function Search() {
+  return (
     <div>
-      <h1>Hello React!</h1>
+      <input className="search" type="text" placeholder="Search movies..." />
+    </div>
+  );
+}
+function MovieQuantity() {
+  return (
+    <div>
+      <p className="movieNum">
+        Found <strong>X</strong> Movies
+      </p>
     </div>
   );
 }
