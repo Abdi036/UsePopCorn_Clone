3572d7ca0f7d959bc51d6c1166127f0e6f6bb698
Abdi Kumela <abdikumela036@gmail.com>
2024-03-20 00:13:40 +0300

    check

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import RatingStar from "./RatingStar";
+// import RatingStar from "./RatingStar";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
-    <RatingStar maxRating={5} />
+    {/* <RatingStar maxRating={5} /> */}
   </React.StrictMode>
 );
