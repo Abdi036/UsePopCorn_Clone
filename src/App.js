a3a2f08852e3970122050ec32dcef19a4550a7dd
Abdi Kumela <abdikumela036@gmail.com>
2024-03-20 20:59:42 +0300

    rendering movie id to Watched movies

--- src/App.js
+++ src/App.js
@@ -1,60 +1,18 @@
 //  http://www.omdbapi.com/?i=tt3896198&apikey={key}
 import { useEffect, useState } from "react";
 
-// const tempMovieData = [
-//   {
-//     imdbID: "tt1375666",
-//     Title: "Inception",
-//     Year: "2010",
-//     Poster:
-//       "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
-//   },
-//   {
-//     imdbID: "tt0133093",
-//     Title: "The Matrix",
-//     Year: "1999",
-//     Poster:
-//       "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
-//   },
-//   {
-//     imdbID: "tt6751668",
-//     Title: "Parasite",
-//     Year: "2019",
-//     Poster:
-//       "https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg",
-//   },
-// ];
-
-// const tempWatchedData = [
-//   {
-//     imdbID: "tt1375666",
-//     Title: "Inception",
-//     Year: "2010",
-//     Poster:
-//       "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
-//     runtime: 148,
-//     imdbRating: 8.8,
-//     userRating: 10,
-//   },
-//   {
-//     imdbID: "tt0088763",
-//     Title: "Back to the Future",
-//     Year: "1985",
-//     Poster:
-//       "https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
-//     runtime: 116,
-//     imdbRating: 8.5,
-//     userRating: 9,
-//   },
-// ];
-
 const key = "fd9ff071";
 export default function App() {
+  const [selectedId, setSelectedId] = useState(null);
   const [query, setQuery] = useState("");
   const [watchedMovies, setWatchedMovies] = useState([]);
   const [movies, setMovies] = useState([]);
   const [isfetching, setIsFetching] = useState(false);
   const [error, setError] = useState("");
+
+  function handleSelectedId(id) {
+    setSelectedId((selectedId) => (id === selectedId ? null : id));
+  }
 
   useEffect(
     function movieData() {
@@ -72,6 +30,7 @@ export default function App() {
           const data = await res.json();
           if (data.Response === "False") throw new Error("Movie Not Found!");
           setMovies(data.Search);
+          console.log(data.Search);
         } catch (err) {
           console.error(err.message);
           setError(err.message);
@@ -98,15 +57,30 @@ export default function App() {
         <Search query={query} setQuery={setQuery} />
         <MovieQuantity movies={movies} />
       </Navbar>
-      <MainContainer>
-        <Box>
+      <MainContainer OnhandleSelectedID={handleSelectedId}>
+        <Box OnhandleSelectedID={handleSelectedId}>
           {isfetching && <Loader />}
-          {!isfetching && !error && <MovieLists movies={movies} />}
+          {!isfetching && !error && (
+            <MovieLists
+              movies={movies}
+              setSelectedId={setSelectedId}
+              OnhandleSelectedID={handleSelectedId}
+            />
+          )}
           {error && <ErrorMessage message={error} />}
         </Box>
         <Box>
-          <Moviesummary />
-          <WatchedMovies watchedMovies={watchedMovies} />
+          {selectedId ? (
+            <MovieDetails
+              selectedId={selectedId}
+              setSelectedId={setSelectedId}
+            />
+          ) : (
+            <>
+              <Moviesummary />
+              <WatchedMovies watchedMovies={watchedMovies} />
+            </>
+          )}
         </Box>
       </MainContainer>
     </>
@@ -179,11 +153,15 @@ function Box({ children }) {
   );
 }
 
-function MovieLists({ movies }) {
+function MovieLists({ movies, OnhandleSelectedID }) {
   return (
     <>
       {movies.map((movie, index) => (
-        <div className="movieList" key={index}>
+        <div
+          className="movieList"
+          key={index}
+          onClick={() => OnhandleSelectedID(movie.imdbID)}
+        >
           <div>
             <img src={movie.Poster} alt={`${movie.Title} poster`} />
           </div>
@@ -228,6 +206,17 @@ function Moviesummary() {
         <span>🌟 9.9</span>
         <span>⏳ 120 min</span>
       </div>
+    </div>
+  );
+}
+
+function MovieDetails({ selectedId, setSelectedId }) {
+  return (
+    <div className="btn_container">
+      {selectedId}
+      <button className="close_btn" onClick={() => setSelectedId(null)}>
+        &larr;
+      </button>
     </div>
   );
 }
